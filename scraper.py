54ba8e0d5cb22040cfea7a9b910ffe8cf3c03cd5
Otto <olarsen22@gmail.com>
2023-04-11 23:56:43 +0200

    fix csv file creation

--- scraper.py
+++ scraper.py
@@ -1,15 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import os
 
-# Define the URLs of the web pages to crawl
+# URls to scrape from
 urls = ['https://www.dtu.dk/uddannelse/ansoegning-og-optagelse/diplomingenioer-bachelor-optagelse/adgangskvotienter?accordion=0',
         'https://www.sdu.dk/da/uddannelse/bachelor/gennemsnit']
 
+# Specify the workign directory and the name of the CSV file to store the collected grades
+directory = r'C:\Users\olars\Documents\code_projects\WebScraper'
+filename = 'entrance_grades.csv'
+
 # Create a CSV file to store the collected grades
-csv_file = open('entrance_grades.csv', 'w', newline='')
-csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['University', 'Program', 'Entrance Grade'])  # Write header row
+csv_file_path = os.path.join(directory, filename)
+os.makedirs(directory, exist_ok=True)
+
+# write header row to the CSV file
+with open(csv_file_path, 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    # Write header row
+    csv_writer.writerow(['University', 'Program', 'Entrance Grade'])
 
 # Loop through the URLs and collect grades
 for url in urls:
@@ -17,7 +27,6 @@ for url in urls:
     soup = BeautifulSoup(response.content, 'html.parser')
 
     # Extract grades from specific HTML tags or attributes
-    # Note: You need to inspect the HTML structure of the web pages to identify the correct tags, classes, or attributes
     grades = soup.find_all('span', {'class': 'grade'})
 
     # Extract additional information as needed, such as university name or program name
@@ -30,3 +39,4 @@ for url in urls:
 
 csv_file.close()
 print("Entrance grades have been collected and saved to 'entrance_grades.csv'")
+print(f"CSV file created at: {os.path.abspath(csv_file_path)}")
